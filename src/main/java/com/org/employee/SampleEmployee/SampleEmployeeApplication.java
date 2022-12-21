fbe3e3e0bd22c6a25452be460af2db34db1e928d
nagendra <nagendra@vibhotech.com>
2022-12-21 23:31:00 +0530

    created Project

---
+++ src/main/java/com/org/employee/SampleEmployee/SampleEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.org.employee.SampleEmployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleEmployeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/org/employee/SampleEmployee/SampleEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.employee.SampleEmployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
